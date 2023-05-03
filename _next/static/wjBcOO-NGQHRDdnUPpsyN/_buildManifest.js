b81f3f6c3dbb3c4fa61fc43065bf02236d61bc6b
nattb8 <nf1993@gmail.com>
2023-05-03 07:48:51 +0000

    Deploying to public from @ nattb8/nattb8.github.io@90495165d2ff55e8fb33792dd2a05dcb55f8198c 🚀

---
+++ _next/static/wjBcOO-NGQHRDdnUPpsyN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c3cdd59d08a7e09c.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
