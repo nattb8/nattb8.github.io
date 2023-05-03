eff3884a3f156f85da603cbb262d745d3680e204
nattb8 <nf1993@gmail.com>
2023-05-03 22:56:20 +0000

    Deploying to public from @ nattb8/nattb8.github.io@d348898a5da4d43232628a389efeff8718282e28 🚀

--- _next/static/fA-pan7o_QF5EdXfTKwBt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5d00f9e3a5a6d7b2.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/login/callback":["static/chunks/pages/login/callback-113e0c97f5fc87bc.js"],sortedPages:["/","/_app","/_error","/login/callback"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
